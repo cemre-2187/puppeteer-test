9e787c7933896a8dd4160ea98d01950ba7f617be
AfraGngr <67650031+AfraGngr@users.noreply.github.com>
2022-11-22 16:51:43 +0300

    UPDATE

--- index.js
+++ index.js
@@ -2,51 +2,93 @@ const puppeteer = require("puppeteer")
 
 let test = async () => {
   const browser = await puppeteer.launch({
+    headless: false,
     args: ['--no-sandbox'],
     timeout: 10000,
   });
   const page = await browser.newPage();
 
-  await page.goto('https://amazon.com');
+  await page.goto('https://amazon.com/dp/B09YRR2LS5');
+
+
+  const subscribeDiv = '#buyBoxAccordion'
+  const subscribe = await page.$(subscribeDiv)
+
+
+  // const subscribeDiv = '#buyBoxAccordion'
+  const oneTime = '#newAccordionRow > div > div.a-accordion-row-a11y.a-accordion-row.a-declarative.accordion-header.mobb-header-css > i'
+  const addToCartBtn = '#add-to-cart-button'
+  const goToCartBtn = '#sw-gtc'
+  const qtyBtn = '#quantity'
+  const qtyTen = '#quantity_10'
+  const qtyInput = 'input[name="quantityBox"]'
+  const updateMsgContent = 'div.sc-quantity-update-message.a-spacing-top-mini > div > div > div > span'
+
+  if (subscribe) {
+    console.log('Subs var')
+    await page.click('#newAccordionRow > div > div.a-accordion-row-a11y.a-accordion-row.a-declarative.accordion-header.mobb-header-css > i')
+  } else {
+    console.log('YOK')
+    await page.waitForSelector(addToCartBtn)
+    await page.click(addToCartBtn, { delay: 2000 })
+    await page.waitForSelector(goToCartBtn)
+    await page.click(goToCartBtn)
+    await page.waitForSelector(qtyBtn)
+    await page.click(qtyBtn)
+    await page.waitForSelector(qtyTen)
+    await page.click(qtyTen)
+    await page.waitForSelector(qtyInput)
+    await page.type(qtyInput, '999')
+    await page.keyboard.press('Enter')
+
+    await page.waitForSelector(updateMsgContent)
+    const noAvailable = await page.$(updateMsgContent)
+    console.log(noAvailable)
+
+
+  }
+
+
+
 
   // Type into search box.
   // await page.type('.devsite-search-field', 'Headless Chrome');
 
   // Wait for suggest overlay to appear and click "show all results".
-  const allResultsSelector = '#nav-global-location-popover-link';
-  await page.waitForSelector(allResultsSelector);
-  await page.click(allResultsSelector);
-  //
-  //GLUXZipUpdateInput
+  // const allResultsSelector = '#nav-global-location-popover-link';
+  // await page.waitForSelector(allResultsSelector);
+  // await page.click(allResultsSelector);
+  // //
+  // //GLUXZipUpdateInput
 
-  // Wait for the results page to load and display the results.
-  // const resultsSelector = '.gsc-results .gs-title';
-  // await page.waitForSelector(resultsSelector);
-  await page.waitForSelector("#GLUXZipUpdateInput");
-  // // Extract the results from the page.
-  const links = await page.evaluate(async () => {
+  // // Wait for the results page to load and display the results.
+  // // const resultsSelector = '.gsc-results .gs-title';
+  // // await page.waitForSelector(resultsSelector);
+  // await page.waitForSelector("#GLUXZipUpdateInput");
+  // // // Extract the results from the page.
+  // const links = await page.evaluate(async () => {
 
-    let inputBox = document.getElementById("GLUXZipUpdateInput")
-    inputBox.value = "10001"
+  //   let inputBox = document.getElementById("GLUXZipUpdateInput")
+  //   inputBox.value = "10001"
 
-    //aria-labelledby="GLUXZipUpdate-announce"
-    // let apply = document.querySelectorAll('[aria-labelledby="GLUXZipUpdate-announce"]')[0];
-    setTimeout(async () => {
-      document.querySelector('[data-action="GLUXPostalUpdateAction"]').click()
+  //   //aria-labelledby="GLUXZipUpdate-announce"
+  //   // let apply = document.querySelectorAll('[aria-labelledby="GLUXZipUpdate-announce"]')[0];
+  //   setTimeout(async () => {
+  //     document.querySelector('[data-action="GLUXPostalUpdateAction"]').click()
 
 
-      console.log(document.querySelector('[data-action="GLUXConfirmAction"]'))
-      setTimeout(() => {
-        document.querySelector('[data-action="GLUXConfirmAction"]').click()
-      }, 3000);
-    }, 4000);
-  });
-  await page.waitForNavigation()
-  let country = await page.evaluate(async () => {
+  //     console.log(document.querySelector('[data-action="GLUXConfirmAction"]'))
+  //     setTimeout(() => {
+  //       document.querySelector('[data-action="GLUXConfirmAction"]').click()
+  //     }, 3000);
+  //   }, 4000);
+  // });
+  // await page.waitForNavigation()
+  // let country = await page.evaluate(async () => {
 
-    return document.getElementById("glow-ingress-line2").innerText
-  });
-  console.log(country)
+  //   return document.getElementById("glow-ingress-line2").innerText
+  // });
+  // console.log(country)
   // // Print all the files.
   // console.log(links.join('\n'));
 
--- test.js
+++ test.js
@@ -1,66 +1,201 @@
 const puppeteer = require('puppeteer');
+const cron = require('node-cron');
+
 const product = {};
+
+let searchProductTask;
 
 (async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
-    await page.goto('https://www.amazon.com/dp/B01BIAF5ZK');
+    await page.goto('https://www.amazon.com');
     // await page.reload();
-
-
-
     // ----------------------------- CHANGE LOCATION ----------------------------- //
     // FIXME: ConfirmClose button doesn't work..
     const locationbtn = '#nav-global-location-popover-link'
     const modal = '.a-popover.a-popover-modal.a-declarative'
+    const locationInput = '#GLUXZipUpdateInput'
+    const applyBtn = '#GLUXZipUpdate > span > input'
+
     await page.waitForSelector(locationbtn)
     await page.click(locationbtn)
     await page.waitForSelector(modal)
-    await page.waitForSelector('#GLUXZipUpdateInput')
-    await page.type('#GLUXZipUpdateInput', '10001')
-    await page.click('#GLUXZipUpdate > span > input')
-    // const btn = await page.$('#GLUXConfirmClose')
-    //console.log(btn)
-    await page.waitForSelector('#a-popover-2')
-    await page.click('#GLUXConfirmClose')
-    // await page.evaluate(() => {
-    //     const btn = document.querySelector('#GLUXConfirmClose')
-    //     btn.click()
-    //     console.log(btn)
-    // })
+    await page.waitForSelector(locationInput)
+    await page.waitForTimeout(500)
+    await page.type(locationInput, '10001')
+    await page.click(applyBtn, { delay: 1000 })
+    await page.reload()
+
+
+    let interval = 0;
+    const data = ['B09YRR2LS5', 'B09W18N3GT', 'B00LU4CZP8', 'B07G1VKCND', 'B07R2P8XZ5', 'B09GK6431D', 'B0838177ZX', 'B09H5MKPP4', 'B01K3SOW98', 'B01M2A4IK1', 'B08KHD615K', 'B09BZ4NV2R']
+    // await page.type('#twotabsearchtextbox', 'B09W18N3GT')
+
+
+    const calculateStock = async () => {
+        console.log('Cron is started')
+        let stock;
+        let limit;
+        await page.goto(`https://amazon.com/dp/${data[interval]}`)
+        // await page.waitForNavigation()
+        const subscribeDiv = '#buyBoxAccordion'
+        const oneTime = '#newAccordionRow > div > div.a-accordion-row-a11y.a-accordion-row.a-declarative.accordion-header.mobb-header-css > i'
+        const addToCartBtn = '#add-to-cart-button'
+        const goToCartBtn = '#sw-gtc'
+        const qtyBtn = '#quantity'
+        const qtyTen = '#quantity_10'
+        const qtyInput = 'input[name="quantityBox"]'
+        const updateMsgContent = 'div.sc-quantity-update-message.a-spacing-top-mini > div > div > div > span'
+
+        const subscribe = await page.$(subscribeDiv)
+
+        if (subscribe) {
+            console.log('Subs var')
+            await page.click(oneTime)
+            await page.waitForSelector(addToCartBtn)
+            await page.evaluate(() => document.getElementById('add-to-cart-button').click())
+            // await page.click(addToCartBtn, { delay: 5000 })
+            await page.waitForSelector(goToCartBtn)
+            await page.click(goToCartBtn)
+            await page.waitForSelector(qtyBtn)
+            await page.click(qtyBtn)
+            await page.waitForSelector(qtyTen)
+            await page.click(qtyTen)
+            await page.waitForSelector(qtyInput)
+            await page.type(qtyInput, '999')
+            await page.keyboard.press('Enter')
+            // await page.waitForSelector(updateMsgContent)
+            // const stockContent = await page.$(updateMsgContent)
+            // const value = await page.evaluate(el => el.textContent, stockContent)
+            const noAvailable = await page.$(updateMsgContent)
+            if (noAvailable) {
+                console.log('999 yok')
+                const stockContent = await page.$(updateMsgContent)
+                const value = await page.evaluate(el => el.textContent, stockContent)
+                if (value.includes('limit')) {
+                    console.log('LİMİTLİ')
+                } else {
+                    const matchedArray = value.match(/This seller has only (\d+) of these available/i);
+                    stock = matchedArray[1]
+                    console.log('STOK', stock)
+                }
+            }
+            //console.log('VALUE', value)
+            // console.log('matchd', matchedArray)
+            // await page.evaluate(() => {
+            //     document.querySelector('span.a-size-small.sc-action-delete > span > input').click()
+            // })
+            await page.click('span.a-size-small.sc-action-delete > span > input', { delay: 2000 })
+            console.log(a)
+            // await page.click('span.a-size-small.sc-action-delete > span > input', { delay: 2000 })
+            interval += 1
+
+        } else {
+            await page.waitForSelector(addToCartBtn)
+            await page.click(addToCartBtn, { delay: 2000 })
+            await page.waitForSelector(goToCartBtn)
+            await page.click(goToCartBtn)
+            await page.waitForSelector(qtyBtn)
+            await page.click(qtyBtn)
+            await page.waitForSelector(qtyTen)
+            await page.click(qtyTen)
+            await page.waitForSelector(qtyInput)
+            await page.type(qtyInput, '999')
+            await page.keyboard.press('Enter')
+            // await page.waitForSelector(updateMsgContent)
+            const noAvailable = await page.$(updateMsgContent)
+            if (noAvailable) {
+                console.log('999 yok')
+                const stockContent = await page.$(updateMsgContent)
+                const value = await page.evaluate(el => el.textContent, stockContent)
+                if (value.includes('limit')) {
+                    console.log('LİMİTLİ')
+                } else {
+                    const matchedArray = value.match(/This seller has only (\d+) of these available/i);
+                    stock = matchedArray[1]
+                    console.log('STOK', stock)
+                }
+            }
+
+            //console.log('VALUE', value)
+            // console.log('matchd', matchedArray)
+            await page.click('span.a-size-small.sc-action-delete > span > input', { delay: 2000 })
+            // await page.evaluate(() => document.querySelectorAll('span.a-size-small.sc-action-delete > span > input').click())
+            interval += 1
+
+        }
+
+
+        //await page.waitForTimeout(1000)
+
+    }
+
+    searchProductTask = cron.schedule(`*/20 * * * * *`, () => {
+        console.log('searchProductTask')
+        calculateStock()
+    }
+    )
+
+    searchProductTask.start()
+
+
 
     // ----------------------------- PRODUCTS HAVE ONE SELLER ----------------------------- //
-    // await page.waitForSelector('#dp')
-
-    // const offersBox = await page.$('.olp-text-box');
-
-    // if (!offersBox) {
-    //     // await page.waitForSelector('#add-to-cart-button')
-    //     await page.click('#add-to-cart-button')
-    //     await page.waitForSelector('#attach-sidesheet-view-cart-button')
-    //     await page.evaluate(async () => {
-    //         setTimeout(() => {
-    //             const goToCartBtn = document.querySelector('#attach-sidesheet-view-cart-button > span > input')
-    //             goToCartBtn.click();
-    //             console.log('asdasd')
-    //             const select = document.querySelector('#quantity')
-    //             console.log('SELECT', select)
-    //             select.click()
-    //         }, 2000)
-    //     })
-
-    //     // await page.click('#attach-sidesheet-view-cart-button > span > input')
-    //     // await page.waitForSelector('#quantity')
-    //     // await page.click('#quantity')
-    //     // const goToCart = await page.$('#attach-sidesheet-view-cart-button > span > input')
-    //     // if (goToCart) await page.click('#attach-sidesheet-view-cart-button > span > input')
-    //     // else console.log('ANAN')
-    // }
+})()
 
 
 
-    // await page.waitForSelector('.olp-text-box')
+
+
+
+
+
+
+
+    // // await page.click('#add-to-cart-button')
+    // // await page.waitForNavigation();
+    // // await page.waitForSelector('#desktop-ptc-button-celWidget');
+    // // await page.click('#sw-gtc');
+    // // await page.waitForSelector('#quantity');
+    // // await page.waitForTimeout(1000);
+    // // await page.click('#quantity')
+    // // await page.waitForSelector('#quantity_10', { timeout: 0 })
+    // // await page.click('#quantity_10')
+    // // await page.waitForSelector('.sc-update-quantity-input', { timeout: 0 })
+    // // await page.type('.sc-update-quantity-input', '999');
+    // // // await page.click('#a-autoid-5-announce');
+    // // await page.keyboard.press('Enter');
+    // // await page.waitForSelector('div[data-feature-id="imb-message-container"] > .a-spacing-none.a-spacing-top-mini');
+    // // let element = await page.$('div[data-feature-id="imb-message-container"] > .a-spacing-none.a-spacing-top-mini');
+    // // let value = await page.evaluate(el => el.textContent.trim(), element)
+    // // if (value.includes('this seller has a limit of')) {
+    // //     const quantity = value.substring(value.indexOf('this seller has a limit of' + 25), value.length - 1);
+    // //     product.limit = true;
+    // //     product.quantity = quantity;
+    // // };
+    // // console.log(product);
+    // // // Wait for the results page to load and display the results.
+    // // const resultsSelector = '#sc-important-message-alert #single-imb-message';
+    // // await page.waitForSelector(resultsSelector);
+
+    // // // Extract the results from the page.
+    // // const links = await page.evaluate(resultsSelector => {
+    // //     const text = document.querySelector(resultsSelector).textContent
+    // //     console.log(text)
+    // //     return text
+    // // });
+
+    // // console.log('LINK__: ', links)
+
+    // // await page.waitForSelector('span[data-feature-id="single-imb-message"]', { timeout: 0 })
+    // // const qty = await page.$$eval('span[data-feature-id="single-imb-message"]', el => el)
+    // // console.log('QUANTITY', qty)
+
+
+
+
+      // await page.waitForSelector('.olp-text-box')
     // let element = await page.$('.olp-text-box');
     // let value = await page.evaluate(el => el.textContent.trim(), element)
     // console.log(parseInt(value.substring(value.indexOf('(') + 1, value.indexOf(')') + 1)))
@@ -162,52 +297,3 @@ const product = {};
 
     //     calculateStock()
     // })
-})()
-
-
-
-
-
-
-
-
-
-
-    // // await page.click('#add-to-cart-button')
-    // // await page.waitForNavigation();
-    // // await page.waitForSelector('#desktop-ptc-button-celWidget');
-    // // await page.click('#sw-gtc');
-    // // await page.waitForSelector('#quantity');
-    // // await page.waitForTimeout(1000);
-    // // await page.click('#quantity')
-    // // await page.waitForSelector('#quantity_10', { timeout: 0 })
-    // // await page.click('#quantity_10')
-    // // await page.waitForSelector('.sc-update-quantity-input', { timeout: 0 })
-    // // await page.type('.sc-update-quantity-input', '999');
-    // // // await page.click('#a-autoid-5-announce');
-    // // await page.keyboard.press('Enter');
-    // // await page.waitForSelector('div[data-feature-id="imb-message-container"] > .a-spacing-none.a-spacing-top-mini');
-    // // let element = await page.$('div[data-feature-id="imb-message-container"] > .a-spacing-none.a-spacing-top-mini');
-    // // let value = await page.evaluate(el => el.textContent.trim(), element)
-    // // if (value.includes('this seller has a limit of')) {
-    // //     const quantity = value.substring(value.indexOf('this seller has a limit of' + 25), value.length - 1);
-    // //     product.limit = true;
-    // //     product.quantity = quantity;
-    // // };
-    // // console.log(product);
-    // // // Wait for the results page to load and display the results.
-    // // const resultsSelector = '#sc-important-message-alert #single-imb-message';
-    // // await page.waitForSelector(resultsSelector);
-
-    // // // Extract the results from the page.
-    // // const links = await page.evaluate(resultsSelector => {
-    // //     const text = document.querySelector(resultsSelector).textContent
-    // //     console.log(text)
-    // //     return text
-    // // });
-
-    // // console.log('LINK__: ', links)
-
-    // // await page.waitForSelector('span[data-feature-id="single-imb-message"]', { timeout: 0 })
-    // // const qty = await page.$$eval('span[data-feature-id="single-imb-message"]', el => el)
-    // // console.log('QUANTITY', qty)
